82a79db4bb69351649fdcc3d5ede312a5242fbb2
Julian Martinez <julian.martinez.s@outlook.com>
2021-01-10 10:15:08 -0500

    Complete project draft done.

---
+++ GitHub Followers/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,50 @@
+//
+//  FavoriteCell.swift
+//  GitHub Followers
+//
+//  Created by Julian Martinez on 1/10/21.
+//
+
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+    static let reuseID = "FavoriteCell"
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(favorite: Follower) {
+        usernameLabel.text = favorite.login
+        avatarImageView.downloadImage(from: favorite.avatarUrl)
+    }
+    
+    func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+}
+
--- GitHub Followers/Screens/FavoritesListVC.swift
+++ GitHub Followers/Screens/FavoritesListVC.swift
@@ -8,32 +8,93 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
+    
+    let tableView               = UITableView()
+    var favorties: [Follower]   = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemTeal
-        
-        PersistanceManager.retreiveFavorites { (result) in
-            switch result{
-            case .success(let favorites):
-                print(favorites)
-            case .failure(let error):
-                break
-            }
-        }
-        
+        configureViewController()
+        getFavorties()
+        configureTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorties()
     }
-    */
-
+    
+    func configureViewController() {
+        view.backgroundColor    = .systemBackground
+        title                   = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame         = view.bounds
+        tableView.rowHeight     = 80
+        tableView.delegate      = self
+        tableView.dataSource    = self
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseID)
+    }
+    
+    func getFavorties() {
+        PersistanceManager.retreiveFavorites { [weak self] (result) in
+            guard let self = self else {return}
+            switch result{
+            case .success(let favorites):
+                
+                if favorites.isEmpty {
+                    self.showEmptyStateView(with: "No Favorites?\nAdd one on the follower screen.", in: self.view)
+                } else {
+                    self.favorties = favorites
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        self.view.bringSubviewToFront(self.tableView)
+                    }
+                }
+                
+                self.favorties = favorites
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
 }
+
+extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        favorties.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteCell") as! FavoriteCell
+        let favorite = favorties[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite    = favorties[indexPath.row]
+        let destVC      = FollowersListVC()
+        destVC.username = favorite.login
+        destVC.title    = favorite.login
+        
+        navigationController?.pushViewController(destVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else {return}
+        let favorite = favorties[indexPath.row]
+        favorties.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PersistanceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] (error) in
+            guard let self = self else {return}
+            guard let error = error else {return}
+            self.presentGFAlertOnMainThread(title: "Unable To Remove", message: error.rawValue, buttonTitle: "Ok")
+        }
+    }
+}
+    
